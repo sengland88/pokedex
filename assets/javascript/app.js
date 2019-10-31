77c1b0d2f59840e2aa010edf85725f74976d74ab
Shelby England <Shelby.England@knights.ucf.edu>
2019-10-30 23:00:13 -0400

    add pokemon.js added pokemon api to weather as backup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,26 +7,30 @@
 
 // })
 
+getPokemon()
+getWeather()
+
 
 function getWeather() {
     
-    let theCity = userInput //captures the city variable    
+    // let theCity = userInput //captures the city variable   
+    let  theCity = "orlando"
     let theKey = "0e1a9770bdf84bbcb4d232a7900a26e1" //stores the weather.io api key
     console.log(`${theCity} is the City`) //console.logs the city to make sure that it is working
 
-    let queryURL = "https://api.weatherbit.io/v2.0/current?key=" + theKey + "&city=" + theCity //concatenates all of the information
+    let weatherURL = "https://api.weatherbit.io/v2.0/current?key=" + theKey + "&city=" + theCity //concatenates all of the information
 
     // AJAX request to retrieve data from API
     $.ajax({
-        url: queryURL,
+        url: weatherURL,
         method: "GET"
     }).then(function(response) {
         // console.log(response.data)
 
-        let results = response.data
+        let weatherResults = response.data
 
-        console.log(results) //console.log the whole results
-        console.log(results[0].weather.code) //console.log the current weather's weather.io code
+        console.log(weatherResults) //console.log the whole results
+        console.log(weatherResults[0].weather.code) //console.log the current weather's weather.io code
     })
 
     //weather.io weather codes - to be used for conditional statements (comparing current weather against the type)
@@ -37,5 +41,50 @@ function getWeather() {
     // snow = 600 - 623
     // fog = 741 and 751   
     // windy = 803
+
+}
+
+function getPokemon() {
+    
+    let thePokemon = "rattata"
+    let pokemonURL = "https://pokeapi.co/api/v2/pokemon/" + thePokemon
+
+    $.ajax({
+        url: pokemonURL,
+        method: "GET"
+    }).then(function(response) {
+        console.log(response)
+        
+        let pokemonResults = response
+
+        console.log(pokemonResults.name)
+        console.log(pokemonResults.types[0].type.name)
+        console.log(pokemonResults.sprites.front_default)
+        console.log(pokemonResults.id)
+
+        let id = pokemonResults.id
+
+        getPokemonEvolution(id)
+
+
+
+        //can do shiny pokemon - do have png under sprites
+    })
+
+    
+}
+
+function getPokemonEvolution(evolution) {
+
+    let theID = evolution
+    let pokemonURL = "https://pokeapi.co/api/v2/evolution-chain/"
+
+    $.ajax({
+        url: pokemonURL,
+        method: "GET"
+    }).then(function(response) {
+
+        console.log(response)
+    })
 
 }
