c791bcfa46623535a185bb1aecced79fcb536114
Nick Martello <thenickmartello@gmail.com>
2019-11-06 22:48:49 -0500

    added parallax and made columns mobile friendly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -133,3 +133,11 @@ function getWeather(lat , lon) {
 
     })
 }
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
